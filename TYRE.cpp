cfb53d5df00551d74f4929e62eb8fced6de63fdb
Manav Lukar <91900036+MaNaV-XD@users.noreply.github.com>
2022-03-12 09:38:38 +0530

    Add files via upload

---
+++ TYRE.cpp
@@ -0,0 +1,22 @@
+//  We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<(n*2+m*4)<<endl;
+}
+int main() {
+	// your code goes here
+	int cas;
+	cin>>cas;
+	while(cas--)
+	{
+	solve();
+	}
+	return 0;
+}
